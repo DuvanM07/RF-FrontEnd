b831cf677fd1bb29a48d55c0b2ca9622dc343f74
SaN <duvancoba12@gmail.com>
2025-02-25 19:00:37 -0500

    [+] agregar componentes dashboard y pagenotfound. Define las rutas para todos los componentes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/public/home/home.component';
+import { LoginComponent } from './pages/public/login/login.component';
+import { RegisterComponent } from './pages/public/register/register.component';
+import { CreditsComponent } from './pages/public/credits/credits.component';
+import { DashboardComponent } from './pages/private/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/public/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [ 
+    { path: 'home', component: HomeComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'credits', component: CreditsComponent},
+    { path: 'dashborad', component: DashboardComponent},
+    { path: '404', component: PageNotFoundComponent},
+    { path: 'dashboard/users', component: PageNotFoundComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: '**', redirectTo: '404', pathMatch: 'full' }
+
+];
